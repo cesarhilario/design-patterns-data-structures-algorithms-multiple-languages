1667865365c4c98ee811e880eeb9ef18e074bad2
César Hilário <cesar.hilario.dev@gmail.com>
2023-03-15 00:49:14 -0300

    chore: format file

--- creational/singleton/java/concept/SingletonJavaConcept/src/ClientCode.java
+++ creational/singleton/java/concept/SingletonJavaConcept/src/ClientCode.java
@@ -2,7 +2,7 @@ public class ClientCode {
   public static void main(String[] args) {
     Singleton instanceA = Singleton.getInstance();
     Singleton instanceB = Singleton.getInstance();
-    
+
     if(instanceA == instanceB && instanceA.equals(instanceB)) {
       System.out.println("Singleton works");
     } else {
